68432e8f25b692315735d0426291aea555430fee
Pranav <pranav.techiegeek@gmail.com>
2023-08-28 17:49:41 +0530

    basic change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,26 @@
 <template>
-  <div>
+	<div>
+		<h1>{{ msg }}</h1>
 
-    <h1>{{ msg }}</h1>
-
-    <p>
-      <a
-        href="https://vitejs.dev/guide/features.html"
-        target="_blank"
-      >Vite Documentation</a> |
-      <a
-        href="https://vuejs.org/v2/guide/"
-        target="_blank"
-      >Vue 2 Documentation</a>
-    </p>
-
-    <button @click="count++">count is: {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test hot module replacement.
-    </p>
-  </div>
+		<button @click="count++">count is: {{ count }}</button>
+	</div>
 </template>
 
 <script>
 export default {
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      count: 0,
-    };
-  },
+	props: {
+		msg: String,
+	},
+	data() {
+		return {
+			count: 0,
+		};
+	},
 };
 </script>
 
 <style scoped>
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
